3c3cfdb0762d677d441b9ad75edc0b4768db7fd7
MatteoEtzer <matteoetzer@gmx.at>
2019-12-03 12:59:18 +0100

    added tree, added movement script, added fog and added boundry box

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+                float scroll = Input.GetAxis ("Mouse ScrollWheel");
+
+                transform.LookAt (target);
+                transform.Translate(0, 0, scroll * speed, Space.World);
+
+
+            }
+
+        if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+
+            float scroll = Input.GetAxis ("Mouse ScrollWheel");
+            transform.LookAt (target);
+            transform.Translate(0, 0, scroll * speed, Space.Self);
+        }
+    }
